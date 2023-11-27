5d5b587c1a263208204de5effaec1408bd2af530
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-11-27 16:59:40 +0530

    Add files via upload

---
+++ HealthTrackerApp/DAL/Services/HealthTrackerService.cs
@@ -0,0 +1,46 @@
+﻿using HealthTrackerApp.DAL.Interrfaces;
+using HealthTrackerApp.DAL.Services.Repository;
+using HealthTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace HealthTrackerApp.DAL.Services
+{
+    public class HealthTrackerService : IHealthTrackerService
+    {
+        private readonly IHealthTrackerRepository _repository;
+
+        public HealthTrackerService(IHealthTrackerRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<Health> CreateHealth(Health expense)
+        {
+            return _repository.CreateHealth(expense);
+        }
+
+        public Task<bool> DeleteHealthById(long id)
+        {
+            return _repository.DeleteHealthById(id);
+        }
+
+        public List<Health> GetAllHealths()
+        {
+            return _repository.GetAllHealths();
+        }
+
+        public Task<Health> GetHealthById(long id)
+        {
+            return _repository.GetHealthById(id); ;
+        }
+
+        public Task<Health> UpdateHealth(Health model)
+        {
+            return _repository.UpdateHealth(model);
+        }
+    }
+}
---
+++ HealthTrackerApp/DAL/Services/Repository/HealthTrackerRepository.cs
@@ -0,0 +1,89 @@
+﻿using HealthTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace HealthTrackerApp.DAL.Services.Repository
+{
+    public class HealthTrackerRepository : IHealthTrackerRepository
+    {
+        private readonly DatabaseContext _dbContext;
+        public HealthTrackerRepository(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        public async Task<Health> CreateHealth(Health expense)
+        {
+            try
+            {
+                var result =  _dbContext.Healths.Add(expense);
+                await _dbContext.SaveChangesAsync();
+                return expense;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+        public async Task<bool> DeleteHealthById(long id)
+        {
+            try
+            {
+                _dbContext.Healths.Remove(_dbContext.Healths.Single(a => a.HealthEntryId == id));
+                _dbContext.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+        public List<Health> GetAllHealths()
+        {
+            try
+            {
+                var result = _dbContext.Healths.ToList();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+        public async Task<Health> GetHealthById(long id)
+        {
+            try
+            {
+                return await _dbContext.Healths.FindAsync(id);
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+      
+        
+
+        public async Task<Health> UpdateHealth(Health model)
+        {
+            var ex = await _dbContext.Healths.FindAsync(model.HealthEntryId);
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+                return ex;
+            }
+            catch (Exception exc)
+            {
+                throw (exc);
+            }
+        }
+    }
+}
